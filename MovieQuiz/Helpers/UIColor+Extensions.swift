5fc2b85672cf1482dd393f4cab429d60a35dd61a
Kirill Gontsov <mail@kgontsov.ru>
2023-03-11 15:48:27 +0300

    test

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -2,10 +2,10 @@ import Foundation
 import UIKit
 
 extension UIColor {
-       static var ypBlack: UIColor { UIColor(named: "ypBlack") ?? UIColor.black }
-       static var ypGreen: UIColor { UIColor(named: "ypGreen") ?? UIColor.green }
-       static var ypRed: UIColor { UIColor(named: "ypRed") ?? UIColor.red }
-       static var ypBackground: UIColor { UIColor(named: "ypBackground") ?? UIColor.darkGray }
-       static var ypGray: UIColor { UIColor(named: "ypGray") ?? UIColor.gray }
-       static var ypWhite: UIColor { UIColor(named: "ypWhite") ?? UIColor.white }
+       static var ypBlack: UIColor { UIColor(named: "ypblack") ?? UIColor.black }
+       static var ypGreen: UIColor { UIColor(named: "ypgreen") ?? UIColor.green }
+       static var ypRed: UIColor { UIColor(named: "ypred") ?? UIColor.red }
+       static var ypBackground: UIColor { UIColor(named: "ypbackground") ?? UIColor.darkGray }
+       static var ypGray: UIColor { UIColor(named: "ypgray") ?? UIColor.gray }
+       static var ypWhite: UIColor { UIColor(named: "ypwhite") ?? UIColor.white }
 }
