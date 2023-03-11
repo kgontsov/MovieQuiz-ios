7d8e940d7157097e8fdf73f17625e69417d72a95
Kirill Gontsov <mail@kgontsov.ru>
2023-03-11 16:21:31 +0300

    заменил на force unwrap в uicolor+extention по совету наставника. Переименовал цвета в assets и uicolor+extention с маленькой буквы

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,11 +1,20 @@
 import Foundation
 import UIKit
 
-extension UIColor {
+/* extension UIColor {
        static var ypBlack: UIColor { UIColor(named: "ypblack") ?? UIColor.black }
        static var ypGreen: UIColor { UIColor(named: "ypgreen") ?? UIColor.green }
        static var ypRed: UIColor { UIColor(named: "ypred") ?? UIColor.red }
        static var ypBackground: UIColor { UIColor(named: "ypbackground") ?? UIColor.darkGray }
        static var ypGray: UIColor { UIColor(named: "ypgray") ?? UIColor.gray }
        static var ypWhite: UIColor { UIColor(named: "ypwhite") ?? UIColor.white }
+} */
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "ypblack")! }
+    static var ypWhite: UIColor { UIColor(named: "ypwhite")! }
+    static var ypBackground: UIColor { UIColor(named: "ypbackground")! }
+    static var ypGreen: UIColor { UIColor(named: "ypgreen")! }
+    static var ypRed: UIColor { UIColor(named: "ypred")! }
+    static var ypGray: UIColor { UIColor(named: "ypgray")! }
 }
