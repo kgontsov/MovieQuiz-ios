ebc7b9b28e8976df9dc23c243b5ae25b856394e6
Kirill Gontsov <mail@kgontsov.ru>
2023-03-11 17:18:56 +0300

    убрал лишнее из uicolor+extention, ypBackground все равно остается большим при заливке и я не понимаю почему, сделал его с большой в uicolor+extention, остальные переименовал с маленькой

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,16 +1,6 @@
 import Foundation
 import UIKit
 
-/* extension UIColor {
-       static var ypBlack: UIColor { UIColor(named: "ypblack") ?? UIColor.black }
-       static var ypGreen: UIColor { UIColor(named: "ypgreen") ?? UIColor.green }
-       static var ypRed: UIColor { UIColor(named: "ypred") ?? UIColor.red }
-       static var ypBackground: UIColor { UIColor(named: "ypbackground") ?? UIColor.darkGray }
-       static var ypGray: UIColor { UIColor(named: "ypgray") ?? UIColor.gray }
-       static var ypWhite: UIColor { UIColor(named: "ypwhite") ?? UIColor.white }
-} */
-//q
-
 extension UIColor {
     static var ypBlack: UIColor { UIColor(named: "ypblack")! }
     static var ypWhite: UIColor { UIColor(named: "ypwhite")! }
